003b1e2d51fb31b05d050c836f597123153ca756
George Liu <ji2567389@kimo.com>
2026-08-01 11:15:01 +0800

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        arr = []
+        con_str = ""
+        for i in range(len(s)):
+            if s[i].isalnum():
+                arr.append(s[i].lower())
+        con_str = "".join(arr)
+
+        temp_arr = []
+        temp_str = ""
+        for j in range(len(s)-1,-1,-1):
+            if s[j].isalnum():
+                temp_arr.append(s[j].lower())
+        temp_str = "".join(temp_arr)
+
+        if con_str == temp_str:
+            return True
+        return False
+                
+        
+        
