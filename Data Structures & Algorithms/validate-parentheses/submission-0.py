c3a8b87ae16aede5e5be0fe6d807965b8145b502
George Liu <ji2567389@kimo.com>
2026-08-09 15:10:58 +0800

    Improve parentheses validation logic and comments
    Refactor while loop to include comments for clarity on replacement logic and complexity.

--- Data Structures & Algorithms/validate-parentheses/submission-0.py
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -1,8 +1,8 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        while ("{}" in s) or ("()" in s) or ("[]" in s):
-            s = s.replace("[]","")
-            s = s.replace("{}","")
+        while ("{}" in s) or ("()" in s) or ("[]" in s): 
+            s = s.replace("[]","") # just repalce closely parentheses
+            s = s.replace("{}","") # so complexity N+(N-2)+(N-4)+...+2 = O(N^2)
             s = s.replace("()","")
         if s:
             return False
