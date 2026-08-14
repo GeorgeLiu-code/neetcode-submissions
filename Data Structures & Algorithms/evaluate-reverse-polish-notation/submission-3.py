1f56daeef1b27e9499689566858108a77e57e89a
George Liu <ji2567389@kimo.com>
2026-08-14 13:27:03 +0800

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.py
@@ -0,0 +1,39 @@
+class Double:
+    def __init__(self,val,prev=None,next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        head = Double(tokens[0])
+        curr = head
+        for i in range(1,len(tokens)):
+            curr.next = Double(tokens[i],prev=curr) # to find next node
+            curr = curr.next # find this iteration curr
+        while head is not None:
+            if head.val in "+-*/":
+                left = int(head.prev.prev.val)
+                right = int(head.prev.val)
+                if head.val == "+":
+                    res = left + right
+                elif head.val == "-":
+                    res = left - right
+                elif head.val == "*":
+                    res = left * right
+                else:
+                    res = int(left / right)
+                head.val = str(res)
+                head.prev = head.prev.prev.prev # link previous node
+                
+                if head.prev is not None:
+                    head.prev.next = head # link next node
+            ans  = int(head.val) # must need or head.val = None
+            head = head.next
+        return ans
+
+
+                
+
+
+        
+        
