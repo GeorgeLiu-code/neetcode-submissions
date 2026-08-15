c6046a4a841b64eecd04e5e1953461978cb235ac
George Liu <ji2567389@kimo.com>
2026-08-15 17:49:27 +0800

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # temperatures=[30,38,30,36,35,40,28]
+        # find which have more bigger then clean stack
+        
+        res = [0] * len(temperatures)
+        stack = []
+        for i,n in enumerate(temperatures):
+            while stack and stack[-1][0] < n :
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((n,i))
+        return res
+
+        
